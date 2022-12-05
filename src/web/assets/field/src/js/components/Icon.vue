37ddec234f1e943821feafa2177f09c9bde21f80
Josh Crawford <joshua@sgroup.com.au>
2022-12-06 10:05:56 +1100

    Fix SVG icons not rendering correctly in some instances

--- src/web/assets/field/src/js/components/Icon.vue
+++ src/web/assets/field/src/js/components/Icon.vue
@@ -1,6 +1,6 @@
 <template>
     <template v-if="item.type === 'svg'">
-        <svg v-html="item.displayValue" />
+        <svg v-inline-svg v-html="item.displayValue" />
     </template>
 
     <template v-if="item.type === 'sprite'">
